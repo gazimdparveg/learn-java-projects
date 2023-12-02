5d67af18872f924d641634ff7ec9c2205dd270ad
gazimdparveg <storybook133@gmail.com>
2023-12-02 22:53:27 +0900

    fist java code

---
+++ myjava.js
@@ -0,0 +1,13 @@
+var mu = 1;
+var mu1 = 2;
+total = mu+mu1;
+console.log(total);
+
+var myDate = new Date();
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getUTCFullYear());
+console.log(myDate.getHours());
