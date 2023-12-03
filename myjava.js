64d6ae0b4fe526fec42078ba9cbd9870df64fbb9
gazimdparveg <storybook133@gmail.com>
2023-12-03 20:56:59 +0900

    Right

--- myjava.js
+++ myjava.js
@@ -1,13 +1,16 @@
-var mu = 1;
-var mu1 = 2;
-total = mu+mu1;
-console.log(total);
+// var mu = 1;
+// var mu1 = 2;
+// total = mu+mu1;
+// console.log(total);
 
-var myDate = new Date();
-console.log(myDate.toTimeString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.getDate());
-console.log(myDate.getDay());
-console.log(myDate.getUTCFullYear());
-console.log(myDate.getHours());
+// var myDate = new Date();
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getUTCFullYear());
+// console.log(myDate.getHours());
+
+let myid = document.getElementById("myid");
+myid.innerHTML = "This is Gazi Md Parveg"
